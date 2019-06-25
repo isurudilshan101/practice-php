445d82bdcae741e5199579c730700cedc5f96eab
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2019-06-25 17:50:55 +0530

    intial commit

---
+++ login.php
@@ -0,0 +1,69 @@
+<?php
+
+if(isset($_POST['submit'])){
+
+	//echo "yes we got it";
+
+	//$username=$_POST['username'];
+	//$password=$_POST['password'];
+
+	//if($username&&$password){
+	//echo $username;
+	//echo $password;
+	//}else
+	//echo "noooo";
+
+$connection = mysqli_connect('localhost','root','','loginapp');
+		if($connection)
+
+		{
+			echo "we are connected";
+
+		}else
+  
+		{
+
+			die("database connection failed");
+		}
+
+
+	}
+ 
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+	
+	<div class="col-sm-6">
+		<form action="login.php" method="POST">
+
+			<div class="form-group">
+			<label for="username">Username</label>
+			<input type="text" name="username" class="form-control"></input>
+			</div>
+
+
+			<div class="form-group">
+			<label for="password">Password</label>
+			<input type="password" name="password" class="form-control"></input>
+			</div>
+
+			<input type="submit" class="btn btn-primary" name="submit" value="Submit">
+		</form>
+
+	</div>
+
+
+</div>
+
+</body>
+</html>
